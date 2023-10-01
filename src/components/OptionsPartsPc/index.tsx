2dccbb1ea972e94940336f7d5e690c1310046fe4
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-09-30 22:53:08 -0300

    created components table

--- src/components/OptionsPartsPc/index.tsx
+++ src/components/OptionsPartsPc/index.tsx
@@ -25,6 +25,7 @@ const OptionsPartsPc = () => {
     compatibleHd,
     setCompatibleHd,
     processor,
+    definitiveComponents,
   }: Record<string, any> = useContext(BuildContext);
 
   const handleSelectMotherBoard = (event: Record<string, any>) => {
@@ -126,6 +127,32 @@ const OptionsPartsPc = () => {
           )}
         </>
       )}
+      <S.Table>
+        <S.Tr>
+          <S.Th>Componentes</S.Th>
+          <S.Th>Peças Escolhidas</S.Th>
+        </S.Tr>
+        <S.Tr>
+          <S.Td>Placa Mãe</S.Td>
+          <S.Td>{definitiveComponents[0]}</S.Td>
+        </S.Tr>
+        <S.Tr>
+          <S.Td>Processador</S.Td>
+          <S.Td>{definitiveComponents[1]}</S.Td>
+        </S.Tr>
+        <S.Tr>
+          <S.Td>Memória RAM</S.Td>
+          <S.Td>{definitiveComponents[2]}</S.Td>
+        </S.Tr>
+        <S.Tr>
+          <S.Td>Placa de Vídeo</S.Td>
+          <S.Td>{definitiveComponents[3]}</S.Td>
+        </S.Tr>
+        <S.Tr>
+          <S.Td>HD & SSD</S.Td>
+          <S.Td>{definitiveComponents[4]}</S.Td>
+        </S.Tr>
+      </S.Table>
     </>
   );
 };
--- src/components/OptionsPartsPc/styles.ts
+++ src/components/OptionsPartsPc/styles.ts
@@ -52,3 +52,27 @@ export const FinishButton = styled.button`
     text-decoration: none;
   }
 `;
+
+export const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  margin: 20px 0;
+`;
+
+export const Tr = styled.tr`
+  background-color: #f2f2f2;
+`;
+
+export const Th = styled.th`
+  background-color: #007bff;
+  color: white;
+  padding: 10px;
+  text-align: left;
+`;
+
+export const Td = styled.td`
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+  text-transform: uppercase;
+`;
